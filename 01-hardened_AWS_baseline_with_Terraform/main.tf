b7e8d8c82688d038f2e919cc351ac57104752c93
tiana kaleba andriamanaja <you@example.com>
2026-05-28 18:56:59 +0300

    first implementation ok for the task number 1

--- 01-hardened_AWS_baseline_with_Terraform/main.tf
+++ 01-hardened_AWS_baseline_with_Terraform/main.tf
@@ -1,6 +1,21 @@
 provider "aws" {
   region = "us-east-1"
 }
+# Add this data source
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
@@ -24,7 +39,7 @@ resource "aws_key_pair" "admin" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public.id
   key_name      = aws_key_pair.admin.key_name
@@ -64,6 +79,38 @@ data "aws_caller_identity" "current" {}
 resource "aws_s3_bucket" "trail" {
   bucket = "infralab-cloudtrail-${data.aws_caller_identity.current.account_id}"
 }
+resource "aws_s3_bucket_policy" "trail" {
+  bucket = aws_s3_bucket.trail.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AWSCloudTrailAclCheck"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action   = "s3:GetBucketAcl"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.trail.id}"
+      },
+      {
+        Sid    = "AWSCloudTrailWrite"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action   = "s3:PutObject"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.trail.id}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}
 
 resource "aws_cloudtrail" "main" {
   name                          = "infralab-trail"
@@ -71,4 +118,7 @@ resource "aws_cloudtrail" "main" {
   include_global_service_events = true
   is_multi_region_trail         = true
   enable_log_file_validation    = true
+
+  depends_on = [aws_s3_bucket_policy.trail]
+
 }
